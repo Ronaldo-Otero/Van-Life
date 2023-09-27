c347dae36c04e0a859bd2b3be4c519ad880a30b5
Ronaldo Otero <79810747+Ronaldo-Otero@users.noreply.github.com>
2023-09-27 15:15:37 -0500

    final update with firestore
    final update before push using firestore

--- api.js
+++ api.js
@@ -1,44 +1,94 @@
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+import { initializeApp } from "firebase/app";
+import {
+  getFireStore,
+  collection,
+  doc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJLELwLx2792DuM_FKmHYbp3a9soWAIk4",
+  authDomain: "van-life-d9b78.firebaseapp.com",
+  projectId: "van-life-d9b78",
+  storageBucket: "van-life-d9b78.appspot.com",
+  messagingSenderId: "911517637116",
+  appId: "1:911517637116:web:add12cb62a0babc4e53a79",
+};
+const app = initializeApp(firebaseConfig);
+const db = getFireStore(app);
+
+const vansCollectionRef = collection(db, "vans");
+
+export async function getVans() {
+  const querySnapshot = await getDocs(vansCollectionRef);
+  const dataArr = querySnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  console.log(dataArr);
+  return dataArr;
 }
 
-export async function getHostVans(id) {
-    const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
+export async function getVan(id) {
+  const docRef = doc(db, "vans", id);
+  const vanSnapshot = await getDoc(docRef);
+  return {
+    ...vanSnapshot.data(),
+    id: vanSnapshot.id,
+  };
 }
+// export async function getVans(id) {
+//     const url = id ? `/api/vans/${id}` : "/api/vans"
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//         throw {
+//             message: "Failed to fetch vans",
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+//     const data = await res.json()
+//     return data.vans
+// }
+export async function getHostVans() {
+  const q = query(vansCollectionRef, where("hostId", "==", "123"));
+  const querySnapshot = await getDocs(vansCollectionRef);
+  const dataArr = querySnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return dataArr;
+}
+// export async function getHostVans(id) {
+//   const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
+//   const res = await fetch(url);
+//   if (!res.ok) {
+//     throw {
+//       message: "Failed to fetch vans",
+//       statusText: res.statusText,
+//       status: res.status,
+//     };
+//   }
+//   const data = await res.json();
+//   return data.vans;
+// }
 
 export async function loginUser(creds) {
-    const res = await fetch("/api/login",
-        { method: "post", body: JSON.stringify(creds) }
-    )
-    const data = await res.json()
+  const res = await fetch("/api/login", {
+    method: "post",
+    body: JSON.stringify(creds),
+  });
+  const data = await res.json();
 
-    if (!res.ok) {
-        throw {
-            message: data.message,
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
+  if (!res.ok) {
+    throw {
+      message: data.message,
+      statusText: res.statusText,
+      status: res.status,
+    };
+  }
 
-    return data
+  return data;
 }
